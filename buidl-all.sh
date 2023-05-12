5cb9c43e325b11f14a6115184c9831cc0f126398
knayt <43851402+knayt@users.noreply.github.com>
2023-05-12 13:47:01 +0300

    Update buidl-all.sh

--- buidl-all.sh
+++ buidl-all.sh
@@ -5,16 +5,19 @@ for dir in $basedir/*/  # list subdirectories
 do
     echo -e "\e[101m\e[01;97m- $dir -\e[0m"
     echo -e "\n\n\e[101m\e[01;97m-------------- $dir ----------------\e[0m" >> build.log
-    #docker build $dir &>> build.log
+    echo -e "\nBuilding... See build.log for results\e[0m"
 
     # fix pnpm lock file
     cd $dir
-    pnpm i
+    pnpm i &>> build.log
 
+    img_test_name="get.stopphish.ru:5000/stopphish/${PWD##*/}"
+    echo $img_test_name
 
     # go back and build
     cd $basedir
-    docker build $dir
+    #docker build $dir
+    docker build -t $img_test_name $dir &>> build.log
 done
 
 
